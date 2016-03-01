1a54b6dbd9ca284efb9f85103be7ed1b57503ee5
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-03-01 15:40:15 +0800

    replace local file dir with `__dir__`

--- dump.rb
+++ dump.rb
@@ -3,7 +3,7 @@ pid = ARGV[0]
 filename = ARGV[1]
 
 command = <<-COM
-  bundle exec rbtrace -p #{pid} -e 'Thread.new{GC.start;require "objspace";io=File.open("/home/mz026/codes/codementor/memory-hunting/#{filename}", "w"); ObjectSpace.dump_all(output: io); io.close}'
+  bundle exec rbtrace -p #{pid} -e 'Thread.new{GC.start;require "objspace";io=File.open("#{__dir__}/#{filename}", "w"); ObjectSpace.dump_all(output: io); io.close}'
 COM
 
 system command
